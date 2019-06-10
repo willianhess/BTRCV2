f6cbb1ec88b93be827c5ba58f4977dee8cc31956
Willian Hess <willianhess.tdi@gmail.com>
2019-06-10 00:09:14 -0300

    rest

---
+++ app/src/main/java/de/kai_morich/simple_bluetooth_terminal/MainActivity.java
@@ -0,0 +1,47 @@
+package de.kai_morich.simple_bluetooth_terminal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements FragmentManager.OnBackStackChangedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportFragmentManager().addOnBackStackChangedListener(this);
+        if (savedInstanceState == null)
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment, new DevicesFragment(), "devices").commit();
+        else
+            onBackStackChanged();
+    }
+
+  /*  public void RestServ(View view) {
+
+        Intent intent = new Intent(this, Rest_test.class);
+        startActivity(intent);
+    }*/
+
+    public void restview(View view){
+
+        Intent intent = new Intent(this, Rest_test.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackStackChanged() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(getSupportFragmentManager().getBackStackEntryCount()>0);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
